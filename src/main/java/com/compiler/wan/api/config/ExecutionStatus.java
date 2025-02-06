4ccd4b2dcb9ae660419c7ec131ed19aff564d6e5
jaewan <sjo9810@naver.com>
2025-02-06 16:31:37 +0900

    feat: 실행상태값 추가
    - 실행상태 확인 메소드 추가

--- src/main/java/com/compiler/wan/api/config/ExecutionStatus.java
+++ src/main/java/com/compiler/wan/api/config/ExecutionStatus.java
@@ -3,5 +3,10 @@ package com.compiler.wan.api.config;
 public enum ExecutionStatus {
     SUCCESS,
     COMPILE_ERROR,
-    EXCEPTION
+    SERVER_ERROR,
+    EXCEPTION;
+
+    public static boolean isERROR(ExecutionStatus status) {
+        return status != null && (status.equals(COMPILE_ERROR) || status.equals(SERVER_ERROR));
+    }
 }
