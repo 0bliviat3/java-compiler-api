87ab3a7acc7f45b46dd2de8110a6c4dbf830e41d
jaewan <sjo9810@naver.com>
2025-02-06 16:35:03 +0900

    feat: 컴파일러 컨트롤러 에러확인로직 추가

--- src/main/java/com/compiler/wan/api/controller/CompilerAPIController.java
+++ src/main/java/com/compiler/wan/api/controller/CompilerAPIController.java
@@ -1,16 +1,19 @@
 package com.compiler.wan.api.controller;
 
-import com.compiler.wan.api.config.ExecutionStatus;
-import com.compiler.wan.api.domain.ExecutionResult;
+import com.compiler.wan.api.domain.ExecutionContent;
 import com.compiler.wan.api.service.CompilerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.compiler.wan.api.config.ExecutionStatus.isERROR;
+
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class CompilerAPIController {
 
     private final CompilerService compilerService;
@@ -21,10 +24,14 @@ public class CompilerAPIController {
     }
 
     @PostMapping("/compile")
-    public ExecutionResult compileCode(@RequestBody String code) {
-        ExecutionResult result = compilerService.compileCode(code);
+    public ExecutionContent compileCode(@RequestBody ExecutionContent target) {
 
-        if (result.getStatus().equals(ExecutionStatus.COMPILE_ERROR)) {
+        log.info("code: " + target.getCode());
+
+        ExecutionContent result = compilerService.compileCode(target.getCode());
+
+        if (isERROR(result.getStatus())) {
+            log.info("compile ERROR");
             return result;
         }
 
